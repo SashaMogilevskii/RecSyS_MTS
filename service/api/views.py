85971060e971ff2af4484a0285a6e98707599d09
Sasha_Mogilevskii <mogilevskiy94@mail.ru>
2022-11-23 16:48:59 -0300

    update, add token

--- service/api/views.py
+++ service/api/views.py
@@ -1,10 +1,44 @@
+import os
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, FastAPI, Request, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.api_key import APIKey, APIKeyHeader, APIKeyQuery
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError, ModelNotFoundError
+from service.api.exceptions import (
+    ModelNotFoundError,
+    NotAuthorizedError,
+    UserNotFoundError,
+)
 from service.log import app_logger
+
+load_dotenv()
+
+API_KEY = os.getenv('PROJECT_API_KEY')
+assert API_KEY is not None, 'API_KEY is empty!'
+
+api_key_query = APIKeyQuery(name='API_KEY', auto_error=False)
+api_key_header = APIKeyHeader(name='API_KEY', auto_error=False)
+token_bearer = HTTPBearer(auto_error=False)
+
+
+async def get_api_key(
+    api_key_from_query: str = Security(api_key_query),
+    api_key_from_header: str = Security(api_key_header),
+    token: HTTPAuthorizationCredentials = Security(token_bearer),
+    ) -> str:
+    if api_key_from_query == API_KEY:
+        return api_key_from_query
+    if api_key_from_header == API_KEY:
+        return api_key_from_header
+    if token is not None and token.credentials == API_KEY:
+        return token.credentials
+    raise NotAuthorizedError()
+
+
+
 
 
 class RecoResponse(BaseModel):
@@ -31,12 +65,13 @@ async def health() -> str:
     tags=["Recommendations"],
     response_model=RecoResponse,
     responses={404: {"description": "User not found or model not use"},
-               }
+               401: {"description": "Token is wrong."}}
 )
 async def get_reco(
     request: Request,
     model_name: str,
     user_id: int,
+    api_key: APIKey = Depends(get_api_key)
 ) -> RecoResponse:
     """
     Get recommendations for  a user
